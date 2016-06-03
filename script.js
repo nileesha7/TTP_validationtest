3010ebfb5ae165ff70915ddb7c7776a578f403a2
Nileesha Fernando <nileeshasajani@hotmail.com>
2016-06-03 14:11:30 -0400

    Added email

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.getElementById('Submit').onclick=function(){
 	var userName = document.getElementById('userName').value;
-	var password =  document.getElementById('userName').value;
+	var password =  document.getElementById('password').value;
 
 	var matches =  /\d/.test(userName);
-	if(!matches & password!=12345678){
+	if(!matches){
 		alert("Invalid login!");
 	}
+	else if(password!=12345678){
+		alert("Invalid login");
+	}
+	else{
+		alert("Successful");
+		document.getElementById('h1').innerText="Success";
+	}
+	return false;
 
 }
-document.getElementById('h1').innerText="New Text";
